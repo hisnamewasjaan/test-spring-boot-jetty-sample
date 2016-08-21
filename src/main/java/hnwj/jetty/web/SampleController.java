84efb14957425804b62c93940ded61161069fbf0
Jan Bentzen <jbe@nineconsult.dk>
2016-08-21 20:41:36 +0200

    Added proper index page with links to the various samples

--- src/main/java/hnwj/jetty/web/SampleController.java
+++ src/main/java/hnwj/jetty/web/SampleController.java
@@ -6,8 +6,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * Simple controller with a couple of mappings to '/' and '/other'
@@ -23,13 +27,13 @@ public class SampleController {
 	@Autowired
 	private HelloWorldService helloWorldService;
 
-	@GetMapping("/")
+	@GetMapping("/sample/hello")
 	@ResponseBody
 	public String helloWorld() {
 		return this.helloWorldService.getHelloMessage();
 	}
 
-	@GetMapping("/other")
+	@GetMapping("/other/hello")
 	@ResponseBody
 	public String helloOther() {
 		log.debug("helloOther - debug");
@@ -39,5 +43,16 @@ public class SampleController {
 		return String.format("<html><head><title>%s</title></head><body>%s</body></html>",
 				description,
 				this.helloWorldService.getOtherHelloMessage()) ;
+	}
+
+	@RequestMapping("/")
+	public String s(@RequestParam(name = "name", defaultValue = "n/a") String name) {
+		return "forward:/sample";
+//		return String.format("forward:/sample?name=%s", name);
+	}
+	@RequestMapping("/sample")
+	public String sample(@RequestParam(name = "name", defaultValue = "n/a") String name, Model model) {
+		model.addAttribute("name", name);
+		return "index";
 	}
 }
